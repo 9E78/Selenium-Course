22e557f777aeed49ce575889be13b3a828b7723f
Sofia Getman <Sofia.Getman@broadridge.com>
2017-07-03 20:31:37 +0300

    4.8 Check that all products from main page have stickers

---
+++ selenium_course/l4t8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace selenium_course
+{
+    [TestFixture]
+    public class CheckStickersOnMainPage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void l4t8_CheckStickersOnMainPage()
+        {
+            driver.Url = "http://localhost:100/litecart/";
+
+            foreach (IWebElement productItem in driver.FindElements(By.XPath("//li[@class='product column shadow hover-light']")))
+            {
+                productItem.FindElement(By.XPath("./a/div/div[contains(@class, 'sticker')]"));                
+            }
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
